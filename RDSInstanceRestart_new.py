43ea2c63f2a9130d873618d10e4fd4580c9c68e5
dgoswami <goswamidasharath@gmail.com>
2020-05-16 12:48:34 +0530

    rds restart script added

---
+++ RDSInstanceRestart_new.py
@@ -0,0 +1,33 @@
+
+
+#   Author: Dashrath Goswami
+#   Date:   16th May 2020
+#   Detail: Stop  start and restart RDS instances based on Tags
+#   Note:   Aurora databases are currently not supported for shutdown and startup methods.
+#http://www.mrmarkyoung.com/oracle/2017/12/19/how-to-stop-and-start-rds-instance-in-aws-using-python/
+
+import boto3
+
+Key = 'db-type'
+Value = 'customer-replication'
+
+client = boto3.client('rds')
+response = client.describe_db_instances()
+
+for resp in response['DBInstances']:
+   db_instance_arn = resp['DBInstanceArn']
+
+
+   response = client.list_tags_for_resource(ResourceName=db_instance_arn)
+   for tags in response['TagList']:
+       if tags['Key'] == str(Key) and tags['Value'] == str(Value):
+           status = resp['DBInstanceStatus']
+           InstanceID = resp['DBInstanceIdentifier']
+           print(InstanceID)
+           #print(status)
+           if status == 'available':
+               print("Restarting RDS %s " % InstanceID)
+               client.reboot_db_instance(DBInstanceIdentifier= InstanceID)
+               #print ("Do something with it : %s" % db_instance_arn)
+           else:
+                print("The database is in" + status + " status!")
